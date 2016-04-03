a9d972f8ec551ae3c30409748d869bad0e8accb4
surya1970 <dinesh.uf36@gmail.com>
2016-04-03 15:00:46 -0500

    done coding part
    Thanks

---
+++ dinesh-updated code/src/main/java/com/example/monu/myapplication/Login.java
@@ -0,0 +1,29 @@
+package com.example.monu.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by vilas on 3/5/2016.
+ */
+public class Login extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+    public void register(View v)
+    {
+        Intent redirect = new Intent(Login.this,Register.class);
+        startActivity(redirect);
+
+    }
+    public void log(View v)
+    {
+        Intent redirect = new Intent(Login.this,Log.class);
+        startActivity(redirect);
+
+    }
+}
---
+++ dinesh-updated code/src/main/java/com/example/monu/myapplication/Song.java
@@ -0,0 +1,24 @@
+package com.example.monu.myapplication;
+
+/**
+ * Created by vilas on 2/26/2016.
+ */
+
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+
+}
+
